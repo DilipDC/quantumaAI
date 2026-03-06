877ca11d793a2dcdf742e7795abcc77b69ddec0d
Chethan <dilipdc.123dc@gmail.com>
2026-03-06 21:34:16 +0530

    Update urls.py

--- ai_engine/urls.py
+++ ai_engine/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-path('', views.dashboard, name="dashboard"),
-path('api/process/', views.process_ai, name="process_ai"),
+path('', views.dashboard),
+path('api/process/', views.process_ai),
 ]
