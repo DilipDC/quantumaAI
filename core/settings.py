b41aa0e914c7346911b2bbf247b29ab41f762dcf
Chethan <dilipdc.123dc@gmail.com>
2026-03-06 21:31:34 +0530

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -1,5 +1,6 @@
 
 from pathlib import Path
+import os
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -52,5 +53,6 @@ DATABASES = {
 }
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / "static"]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
