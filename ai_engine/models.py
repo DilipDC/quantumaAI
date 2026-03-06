5ac980ea71294c27c4bc8e66b2ce5dd3b49b7c45
Chethan <dilipdc.123dc@gmail.com>
2026-03-06 21:33:53 +0530

    Update models.py

--- ai_engine/models.py
+++ ai_engine/models.py
@@ -4,7 +4,6 @@ from django.db import models
 class AIInteraction(models.Model):
     user_prompt = models.TextField()
     ai_response = models.TextField()
-    mode = models.CharField(max_length=20)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
