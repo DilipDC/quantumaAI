40a6f1881c1a0e197d1baedff16ed04f1f824067
Chethan <dilipdc.123dc@gmail.com>
2026-03-06 21:35:20 +0530

    Update views.py

--- ai_engine/views.py
+++ ai_engine/views.py
@@ -1,30 +1,29 @@
 
 from django.shortcuts import render
 from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 from .utils import get_quantum_response
 from .models import AIInteraction
 import json
-from django.views.decorators.csrf import csrf_exempt
 
 def dashboard(request):
-    return render(request, 'dashboard.html')
+    return render(request,'dashboard.html')
 
 @csrf_exempt
 def process_ai(request):
 
     if request.method == "POST":
-
         data = json.loads(request.body)
 
         prompt = data.get("prompt")
-        deep = data.get("mode", False)
 
-        result = get_quantum_response(prompt, deep)
+        result = get_quantum_response(prompt)
 
         AIInteraction.objects.create(
             user_prompt=prompt,
-            ai_response=result,
-            mode="Deep" if deep else "Standard"
+            ai_response=result
         )
 
         return JsonResponse({"response": result})
+
+    return JsonResponse({"response":"invalid request"})
